b1bcbedadebc8d8851dd77c9450d452540277ffb
Hemant2335 <knrt73373@gmail.com>
2023-04-20 18:51:56 +0530

    commit1

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,11 @@ const express  = require('express');
 const router =  express.Router();
 const { body, validationResult } = require('express-validator');
 const User  = require('../models/User');
+const bcrypt = require('bcrypt');
+var  jwt = require('jsonwebtoken');
+
+
+const JWT = "NishantanothernameisD@one";
 
 
 //Create a User using PORT: "/api/auth/createuser" . Doesn't Req the Auth and can create a new user.
@@ -27,12 +32,26 @@ router.post('/createuser',[
   //THis is the way to create a new user and this is connected to the mongo db in the index file like we are using the THe User.create 
   //THen it will create a User using the User Schema format .. 
 
+  const salt = await bcrypt.genSalt(10);
+  const secpass  = await bcrypt.hash(req.body.Password , salt) ;
+
   user  = await User.create({
       Name: req.body.Name,
       Email: req.body.Email,
-      Password: req.body.Password,
-    }).then(user => res.json(user)).catch(err=>{console.log("Error Has Recieved"); res.status(500).send("Some Error HAs Occured");
+      Password: secpass,
+    }).then(user=>{
+      const data = {
+        user :{
+          id: user.id
+        }
+      }
+  
+      const authtoken = jwt.sign(data , JWT);
+      res.json({authtoken});
+      }).catch((err)=>{console.log("Error Has Recieved"); res.status(500).send("Some Error HAs Occured");
     })
+
+    
 })
 
 module.exports = router ; 
