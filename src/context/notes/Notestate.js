47db0b454e9230664a3c9eb752c95aa90f03e677
Hemant2335 <knrt73373@gmail.com>
2023-04-28 22:32:36 +0530

    commit

--- src/context/notes/Notestate.js
+++ src/context/notes/Notestate.js
@@ -2,50 +2,68 @@ import notecontext from "./Notecontext";
 import { useState } from "react";
 
 const Notestate = (props)=>{
+
+    const host = "http://localhost:5000"
     
-    const note = [
-        {
-          "_id": "6446885e404fe892c35663ad",
-          "user": "64413bfcd53ba0dc1d5fb0a9",
-          "Title": "Todayisday",
-          "Task": "Will do Everything",
-          "Tag": "Coding",
-          "Date": "2023-04-24T13:47:10.233Z",
-          "__v": 0
-        },
-        {
-          "_id": "6446885e404fe892c35663ad",
-          "user": "64413bfcd53ba0dc1d5fb0a9",
-          "Title": "Todayisday",
-          "Task": "Will do Everything",
-          "Tag": "Coding",
-          "Date": "2023-04-24T13:47:10.233Z",
-          "__v": 0
-        },
-        {
-          "_id": "6446885e404fe892c35663ad",
-          "user": "64413bfcd53ba0dc1d5fb0a9",
-          "Title": "Todayisday",
-          "Task": "Will do Everything",
-          "Tag": "Coding",
-          "Date": "2023-04-24T13:47:10.233Z",
-          "__v": 0
-        },
-        {
-          "_id": "6446885e404fe892c35663ad",
-          "user": "64413bfcd53ba0dc1d5fb0a9",
-          "Title": "Todayisday",
-          "Task": "Will do Everything",
-          "Tag": "Coding",
-          "Date": "2023-04-24T13:47:10.233Z",
-          "__v": 0
-        },
-      ]
+    let note = [ ]
 
     const [notes, setnotes] = useState(note)
 
+    //Get All Notes
+    const getnote=async()=>{
+      //Todo Api call
+      //Api Call
+    const response = await fetch(`${host}/api/v1/auth/fetchallnotes`, {
+      method: "GET", 
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": localStorage.getItem('token')
+      },
+    });
+    const json =  await response.json()
+    setnotes(json)
+  }
+
+    //Add a note
+    const addnote=async(Title , Task , Tag )=>{
+
+
+        //Todo Api call
+        //Api Call
+      const response = await fetch(`${host}/api/v1/auth/addnote`, {
+        method: "POST", 
+        headers: {
+          "Content-Type": "application/json",
+          "auth-token": localStorage.getItem('token')
+        },
+        body: JSON.stringify({Title , Task , Tag}),
+      });
+
+      const note  = response.json()
+      setnotes(notes.concat(note))
+
+        
+    }
+
+    //Delete a Note
+    const deletenote = async(id)=>{
+
+      //Api Call
+      const response = await fetch(`${host}/api/v1/auth/deletenote/${id}`, {
+        method: "DELETE", 
+        headers: {
+          "Content-Type": "application/json",
+          "auth-token": localStorage.getItem('token')
+        },
+      });
+      //Logic to delete note frontend
+        console.log("Deleting the Note of " + id);        
+        const newnote = notes.filter((note)=>{return note._id!==id})
+        setnotes(newnote)
+    }
+
     return (
-        <notecontext.Provider value  = {{notes,setnotes}}>
+        <notecontext.Provider value  = {{notes,addnote , deletenote , getnote}}>
             {props.children}
         </notecontext.Provider>
 
